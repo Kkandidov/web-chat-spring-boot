da7c39f14e58030e77c6220a53a34787f4216ae0
Konstantin <kkandidovvv@gmail.com>
2020-09-06 17:32:50 +0300

    init the simple spring boot project

---
+++ src/main/java/org/astashonok/webchatspringboot/WebChatSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.astashonok.webchatspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/astashonok/webchatspringboot/WebChatSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.astashonok.webchatspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
